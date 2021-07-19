5e0204289d20997f8853e2e8d053b58b45ab6bb9
ericsayegh <ericsayegh@knights.ucf.edu>
2021-07-19 00:01:18 -0400

    Started settings page
    - Setting page UI set up

--- fitness_app_development/lib/pages/settings.dart
+++ fitness_app_development/lib/pages/settings.dart
@@ -1,4 +1,7 @@
+import 'package:fitness_app_development/services/global_data.dart';
 import 'package:flutter/material.dart';
+import 'package:fitness_app_development/services/global_data.dart';
+import 'package:fitness_app_development/services/get_api.dart';
 
 class Settings extends StatefulWidget {
 
@@ -7,23 +10,91 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  String username = '';
+
+
+  @override
+  void initState() {
+    changeUser();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('Settings'),
       ),
-      body: Container(
-        child: ElevatedButton(
-          child: Text('LOGOUT'),
-          onPressed: (){
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 30.0),
+            Center(
+              child: CircleAvatar(// can add link to users profile pictures for this
+                backgroundImage: NetworkImage(// PLACEHOLDER //
+                    'https://post.greatist.com/wp-content/uploads/2020/01/Runner-training-on-running-track-732x549-thumbnail.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {
 
-            Navigator.of(context).popUntil((route) => route.isFirst);
-          },
+                },
+                child: Text("Change Profile Picture"),
+              ),
+            ),
+
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.mail),
+                    title: Text('Change Email'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.portrait),
+                    title: Text('Change Username'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.lock_outline),
+                    title: Text('Change Password'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: Text('LOGOUT'),
+              onPressed: (){
+
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              },
 
 
+            ),
+          ],
         ),
       ),
     );
   }
+  changeUser(){
+    setState(() {
+      username = GlobalData.firstName!; // need to change to full name
+    });
+  }
+
 }
